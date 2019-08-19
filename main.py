43c8cc310ee86eb2d9fe4fddc6ae2e0a42be0563
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-08-19 03:31:56 -0700

    stays stable

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ from time import sleep
 import cvxpy as cp
 import numpy as np
 
+import scipy.linalg
+ 
+def lqr(A,B,Q,R):
+    """Solve the continuous time lqr controller.
+    dx/dt = A x + B u
+    cost = integral x.T*Q*x + u.T*R*u
+    """
+    X = np.array(scipy.linalg.solve_continuous_are(A, B, Q, R))
+    K = np.array(scipy.linalg.inv(R)@(B.T@X))
+    eigVals, eigVecs = scipy.linalg.eig(A-B@K)
+    return K, X, eigVals
+ 
+def dlqr(A,B,Q,R):
+    """Solve the discrete time lqr controller.
+    x[k+1] = A x[k] + B u[k]
+    cost = sum x[k].T*Q*x[k] + u[k].T*R*u[k]
+    """
+    X = np.array(scipy.linalg.solve_discrete_are(A, B, Q, R))
+    K = np.array(scipy.linalg.inv(B.T@X@B+R)@(B.T@X@A))
+    eigVals, eigVecs = scipy.linalg.eig(A-B@K)
+    return K, X, eigVals
+
 d = Display()
 
 # init = [0,0]
@@ -16,6 +38,18 @@ def main():
     T = 75
     N = p.N
     M = p.M
+
+    target = np.array([0,0,0,np.pi])
+    A, B = p.linearize(target, np.zeros((M)))
+    Qf = 1000*np.eye(N)
+    Qf[0,0] = 10
+    Qf[1,1] = 10
+
+    R  = 1*np.eye(M)
+    K,_,_ = dlqr(A,B,Qf,R)
+    print("K", K)
+    print("A", A)
+    print("B", B)
 
     sleep(1)
 
@@ -45,11 +79,17 @@ def main():
             sleep(0.002)
             d.update(p.state[1],p.state[3])
 
+        for i in range(50):
+            u = np.clip(K @ (p.state - target),-1,1)
+            p.next(-u)
+            sleep(0.01)
+            d.update(p.state[1],p.state[3])
+
         X_p = X + states
         U_p = U + control
 
-        const.append( U_p <=    2)
-        const.append( U_p >= -  2)
+        const.append( U_p <=    1)
+        const.append( U_p >= -  1)
         const.append( X_p[:,1] <=  2)
         const.append( X_p[:,1] >= -2)
 
@@ -63,18 +103,15 @@ def main():
                 # 1* cp.sum((U_p)**2)
                 # 0.1* cp.sum((U_p[1:] - U_p[:-1])**2)
 
-        cost = 1000*cp.sum((X_p[-3:,3] - np.pi)**2) + \
-               1000*cp.sum((X_p[-3:,2]   )**2) +\
-               1000*cp.sum((X_p[-3:,0]   )**2) +\
-               1000*cp.sum((X_p[-3:,1]   )**2) +\
-                10* cp.sum((X_p[:,0]- np.pi)**2) + \
-                10*cp.sum((X_p[:,2]   )**2) +\
-                 2*cp.sum((X_p[:,0]   )**2) +\
-                 2*cp.sum((X_p[:,1]   )**2) +\
-                 1*cp.sum((U_p[:,0]   )**2) +\
+        cost =1000* cp.sum((X_p[-1,:]  - target)**2) + \
+                1000* cp.sum((X_p[-2,:]  - target)**2) + \
+                1000* cp.sum((X_p[-3,:]  - target)**2) + \
+                10* cp.sum((X_p[:,3]   -  np.pi)**2) + \
+                10* cp.sum((X_p[:,2]           )**2) +\
+                 2* cp.sum((X_p[:,1]           )**2) +\
+                 2* cp.sum((X_p[:,0]           )**2) +\
+                 1* cp.sum((U_p[:,0]           )**2) +\
                 0
-
-
 
         prob = cp.Problem( cp.Minimize( cost), const)
         prob.solve()
